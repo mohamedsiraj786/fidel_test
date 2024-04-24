e5f383f0a124b2846bee727e570954b6f11f3e00
FidelCastro007 <118585134+FidelCastro007@users.noreply.github.com>
2024-04-24 10:45:24 +0530

    Create db.js

---
+++ db.js
@@ -0,0 +1,18 @@
+ const mongoose = require('mongoose');
+require('dotenv').config()
+
+const connectDB = async () => {
+    try {
+      await mongoose.connect(process.env.MONGOOSE_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      });
+      console.log('Connected to MongoDB');
+    } catch (error) {
+      console.error('Error connecting to MongoDB:', error);
+      process.exit(1); // Exit process with failure
+    }
+  };
+  
+
+  module.exports = connectDB
